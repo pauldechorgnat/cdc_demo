cf6bce5a8e31ce82e24f1c126fa044561bfc1e77
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2022-10-05 13:36:21 +0200

    Ajout des deux premieres routes de l'API data

--- api/main.py
+++ api/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from typing import List
 from flair.models import SequenceTagger
 from .config import ANONYMIZED_TAGS_DESCRIPTION, ANONYMIZED_TAGS
 from .model import get_entities, anonymize_sentence, replace_text
+from pymongo import MongoClient
+import datetime
+from .utils import format_object_id
+import bson
 
 tagger = SequenceTagger.load("ner")
 
@@ -27,7 +31,46 @@ class AnonymizedTextWithTags(BaseModel):
     anonymized_text: str
     tags: List[Tag]
     
+    
+    
+class Article(BaseModel):
+    object_id: str = "633c6473b9daeb8a90169dcb"
+    hash: int = 533794461304174741
+    author: str = "Analysis by Stephen Collinson, CNN"
+    date_published: datetime.datetime = datetime.datetime(2021, 12, 1, 14, 32, 33)
+    section: str = "golf"
+    url:str = "https://www.cnn.com/2021/12/01/golf/tiger-woods-end-of-era-meanwhile-spt-intl/index.html",
+    headline:str = "Tiger Woods: Is this the end of his era? - CNN",
+    keywords:List[str] = ["golf", "tiger", "woods", "end", "era", "cnn"],
+    raw_text:str = "...",
+    automatic_anonymized_text:str = "...",
+    anonymized_text:str = "...",
+    events:dict = {
+        "insertion": {
+            "date": datetime.datetime(2021, 12, 1, 14, 32, 33),
+            "author": "paul_dechorgnat",
+            "mode": "single" # or batch
+            },
+        "automatic_anonymization": {
+            "date": datetime.datetime(2021, 12, 1, 14, 32, 33),
+            "author": "pseudo_model:0.0.1",
+        },
+        "manual_anonymization": {
+            "date": datetime.datetime(2021, 12, 1, 14, 32, 33),
+            "author": "12343534",
+        }
+    }
+    auto_anonymized_tags : List[Tag] = []
+    manual_anonymized_tags: List[Tag] = []
+    source: str = "sport"
+    
+    
 VERSION = "0.0.1"
+
+client = MongoClient()
+db = client["articles"]
+
+CATEGORIES = db.list_collection_names()
 
 
 api = FastAPI(title="Anonymized text", version=VERSION)
@@ -99,3 +142,75 @@ def post_anonymize_sentence(sentence: Sentence):
         tags=[Tag(text=k, tag=v) for k, v in entities.items()],
     )
     return response
+
+
+@api.get(
+    "/data/articles",
+    tags=["Data"],
+    responses={
+        200: {
+            "description":"OK",
+            "model": List[Article]
+        }
+    }
+    )
+def get_articles(
+    category: List[str] = Query(default=None),
+    date_start: datetime.datetime = Query(default=None),
+    date_end: datetime.datetime = Query(default=None),
+    sections: List[str] = Query(default=None),
+):
+
+    collections =  category if category else CATEGORIES
+
+    mongo_filter = {}
+    
+    if sections:
+        mongo_filter["section"] = {"$in": sections}
+        
+    if date_start:
+        if date_end:
+            
+            mongo_filter["date_published"] = {"$gt": date_start, "$lt": date_end}
+        else:
+            mongo_filter["date_published"] = {"$gt": date_start}
+    else:
+        if date_end:
+            mongo_filter["date_published"] = {"$lt": date_end}
+    
+    results = []
+    for c in collections:
+        results.extend(map(format_object_id, db[c].find(filter=mongo_filter)))
+        
+    results = [Article(**r) for r in results]
+    
+    return results
+
+@api.get(
+    "/data/articles/{category}/{object_id}",
+    tags=["Data"],
+    responses={
+        200: {
+            "description":"OK",
+            "model": Article
+        },
+        404: {
+            "description": "Article or category not found"
+        }
+    }
+)
+def get_article(category: str, object_id: str):
+    if category not in CATEGORIES:
+        raise HTTPException(404, detail=f"Category '{category}' not found.")
+    try:
+        result = db[category].find_one({"_id": bson.objectid.ObjectId(object_id)})
+    except bson.errors.InvalidId:
+        raise HTTPException(422, detail=f"Id '{object_id}' is not valid.")
+    
+    if not result:
+        raise HTTPException(404, detail=f"Article with id '{object_id}' not found.")
+    return Article(**result)
+        
+    
+    
+    
---
+++ api/utils.py
@@ -0,0 +1,4 @@
+def format_object_id(record):
+    record["object_id"] = str(record["_id"])
+    return record
+    
